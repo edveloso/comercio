ec833cd5062b7054f0b9c4b3e2e9943ae00e15c8
ednilson.moura <ednilson.moura@SJ216V99S2.lab.infnet.com.br>
2020-02-01 18:25:17 -0300

    banco

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,40 @@
+create database comercio;
+
+show databases;
+
+use comercio;
+create table usuario (
+     id int not null primary key auto_increment,
+     login varchar(80),
+     senha varchar(80)
+);
+
+insert into usuario(login,senha)
+    values ('admin', 'admin');
+
+select * from usuario;
+
+create table produto (
+  id int not null primary key auto_increment,
+  foto varchar(80),
+  nome varchar(80),
+  descricao varchar(255),
+  preco double(10,2)
+);
+select * from  produto;
+
+insert into produto (foto, nome, descricao, preco)
+    values(
+		'iphone.jpeg',
+        'Iphone',
+        'Design inovador, totalmente em vidro. A câmera que o mundo inteiro adora, ainda melhor. O chip mais poderoso e inteligente em qualquer smartphone. Recarga sem fio simples de verdade.',
+        3099.00
+        );
+        
+insert into produto (foto, nome, descricao, preco)
+    values(
+		'cart.png',
+        'Carrinho de Bebê Agile Cinza',
+        'O Carrinho Multikids tem estrutura de alumínio, por isso é mais leve, resistente e versátil. Possui cinto de segurança de 5 pontos, capota retrátil com visor e tela, 3 rodas com travas e freios.',
+        355.70
+        );        
